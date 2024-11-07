958d0862f4c774e520e3f2d4536992400764cf92
Stylianos <stylgrig@yahoo.gr>
2024-11-07 13:17:16 +0200

    Pilot study with Thijs data

--- Lib_squats.py
+++ Lib_squats.py
@@ -178,10 +178,6 @@ def pink_noise_x_and_y(N):
 
     return x_data, y_data
 
-def normalize_quaternion(q):
-    norm = np.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2)
-    return [q[i] / norm for i in range(4)]
-
 def convert_force_values(data_point):
     """
     This function takes data points of Kinvent force and returns force in kg
@@ -211,6 +207,13 @@ def convert_ang_vel(vel):
     vel = (vel - 32768) * (2000.0/32768)
     return vel
 
+def convert_magnetic_field_density(mag):
+    """
+    This function takes data points of Kinvent IMU angular velocity and returns angular velocity in the range of +-2000°/s
+    """
+    mag = (mag - 32768) * (2500.0/32768)
+    return mag
+
 def q_to_ypr(q):
     """ Import q as a list of quaternion"""
     if q:
@@ -221,3 +224,48 @@ def q_to_ypr(q):
         roll = math.degrees(roll)
         pitch = math.degrees(pitch)
         return [yaw, pitch, roll]
+    
+def convert_KIVNENT_IMU_to_readable_file(df):
+    columns_IMU = ['Time', 'Q_0', 'Q_1', 'Q_2', 'Q_3', 'Acc_X', 'Acc_Y', 'Acc_Z', 'Ang_Vel_X', 'Ang_Vel_Y', 'Ang_Vel_Z',
+                   'Mag_X', 'Mag_Y', 'Mag_Z']
+    df = df.iloc[:, :-1]
+    df.columns = columns_IMU
+    df['Q_0'] = df['Q_0'].apply(convert_quaternion)
+    df['Q_1'] = df['Q_1'].apply(convert_quaternion)
+    df['Q_2'] = df['Q_2'].apply(convert_quaternion)
+    df['Q_3'] = df['Q_3'].apply(convert_quaternion)
+
+    df['Acc_X'] = df['Acc_X'].apply(convert_acceleration)
+    df['Acc_Y'] = df['Acc_Y'].apply(convert_acceleration)
+    df['Acc_Z'] = df['Acc_Z'].apply(convert_acceleration)
+
+    df['Ang_Vel_X'] = df['Ang_Vel_X'].apply(convert_ang_vel)
+    df['Ang_Vel_Y'] = df['Ang_Vel_Y'].apply(convert_ang_vel)
+    df['Ang_Vel_Z'] = df['Ang_Vel_Z'].apply(convert_ang_vel)
+
+    df['Mag_X'] = df['Mag_X'].apply(convert_magnetic_field_density)
+    df['Mag_Y'] = df['Mag_Y'].apply(convert_magnetic_field_density)
+    df['Mag_Z'] = df['Mag_Z'].apply(convert_magnetic_field_density)
+
+    yaw = []
+    pitch = []
+    roll = []
+    for i in range(len(df['Q_0'])):
+        q = []
+        q.append(df['Q_0'][i])
+        q.append(df['Q_1'][i])
+        q.append(df['Q_2'][i])
+        q.append(df['Q_3'][i])
+
+        angles = lib.q_to_ypr(q)
+        yaw.append(angles[0])
+        pitch.append(angles[1])
+        roll.append(angles[2])
+    df['Yaw'] = yaw
+    df['Pitch'] = pitch
+    df['Roll'] = roll
+
+    return df
+    
+
+    
