31da74bd0043ec741ae3afb1a62eda9699c661a6
Stylianos Grigoriadis <stylgrig@yahoo.gr>
2025-02-12 12:08:04 +0200

    Data analysis pipeline

---
+++ User creation of signals.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.widgets import Slider
+import colorednoise as cn
+from fathon import fathonUtils as fu
+import fathon
+import lib
+import lib_squats as lbs
+
+
+
+number_of_data_points = 30
+x_data_sine, y_data_sine = lbs.creation_rigid_signal(number_of_data_points)
+x_data_random, y_data_random = lbs.creation_white_noise(number_of_data_points)
+x_data_6, y_data_6 = lbs.pink_noise_x_and_y(number_of_data_points)
+
+
+plt.scatter(x_data_sine, y_data_sine, label='sine', c='red', lw=5)
+plt.scatter(x_data_random, y_data_random, label='random', c='k')
+plt.scatter(x_data_6, y_data_6, label='pink', c='pink')
+plt.legend()
+plt.show()
+
+
+
+
+
---
+++ User data_analysis.py
@@ -0,0 +1,36 @@
+import lib_squats as lbs
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from matplotlib.widgets import Slider
+import glob
+
+
+pd.set_option("display.max_rows", None)
+
+
+directory_path = r'C:\Users\Stylianos\OneDrive - Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης\My Files\PhD\Projects\Squat Game\Pilot Study 6\Data'
+files = glob.glob(os.path.join(directory_path, "*"))
+for file in files:
+    os.chdir(file)
+    ID = os.path.basename(file)
+    print(ID)
+    if not ID[:-1] == 'static':
+        targets = pd.read_excel(rf'{ID}.xlsx')
+        data = pd.read_csv(rf'{ID}.txt')
+    else:
+        targets = pd.read_excel(rf'{ID[:-1]}.xlsx')
+        data = pd.read_csv(rf'{ID}.txt')
+
+    target_signal_x, target_signal_y = lbs.convert_excel_to_screen_size_targets(targets)
+    target_pos_x, target_pos_y, player_pos_x, player_pos_y = lbs.values_during_game(data)
+    target_pos_x, target_pos_y, player_pos_x, player_pos_y = lbs.return_the_values_before_target_change(target_pos_x, target_pos_y, player_pos_x, player_pos_y)
+    # lbs.graph_creation_target_vs_player(target_pos_x, target_pos_y, player_pos_x, player_pos_y)
+
+    lbs.graph_creation_of_spatial_error(target_pos_x, target_pos_y, player_pos_x, player_pos_y)
+
+
+
+
+
