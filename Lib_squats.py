f1a420160d8aae500f7fee7d90926d68789a8bc0
Stylianos Grigoriadis <stylgrig@yahoo.gr>
2025-02-12 10:42:12 +0200

    Data analysis and creation of all signals

--- Lib_squats.py
+++ Lib_squats.py
@@ -8,17 +8,15 @@ import fathon
 import lib
 from scipy.constants import g
 import math
-from scipy import stats
-from scipy.stats import pearsonr
 
 
 def DFA(variable):
     a = fu.toAggregated(variable)
-    # b = fu.toAggregated(b)
+        #b = fu.toAggregated(b)
 
     pydfa = fathon.DFA(a)
 
-    winSizes = fu.linRangeByStep(start=4, end=int(len(variable) / 4))
+    winSizes = fu.linRangeByStep(start=4, end=int(len(variable)/4))
     revSeg = True
     polOrd = 1
 
@@ -35,14 +33,12 @@ def DFA(variable):
     # plt.show()
     return H
 
-
 def ratio_0_to_100(data_series):
     """ Takes a data series and converts it into values from 0 to 100"""
     data_series = np.array(data_series)
-    data_series = (data_series - np.min(data_series)) / (np.max(data_series) - np.min(data_series)) * 100
+    data_series = (data_series - np.min(data_series)) / (np.max(data_series) - np.min(data_series))*100
 
     return data_series
-
 
 def ratio_0_to_1(data_series):
     """ Takes a data series and converts it into values from 0 to 1"""
@@ -51,53 +47,26 @@ def ratio_0_to_1(data_series):
 
     return data_series
 
-
-def creation_rigid_signal(N):
-    x_sequence = [50, 100, 50, 0]
-    y_sequence = [100, 50, 0, 50]
-    x_data = np.tile(x_sequence, N // len(x_sequence))
-    y_data = np.tile(y_sequence, N // len(y_sequence))
-    more = N - len(x_data)
-    for i in range(0,more):
-        x_data = np.append(x_data, x_sequence[i])
-        y_data = np.append(y_data, y_sequence[i])
-
-
-    return x_data, y_data
-
-
-def creation_white_noise(N):
-    x_data = np.random.rand(N)
-    y_data = np.random.rand(N)
-
-    x_data = ratio_0_to_100(x_data)
-    y_data = ratio_0_to_100(y_data)
-
-    return x_data, y_data
-
-
 def pink_noise_x_y(N):
     """ This function creates a pink noise signal and then appends 1 value to the x_data and 1 value to the y_data"""
-    pink_noise = cn.powerlaw_psd_gaussian(1, N * 2)
+    pink_noise = cn.powerlaw_psd_gaussian(1,N*2)
     pink_noise = ratio_0_to_1(pink_noise)
     x_data = []
     y_data = []
 
-    for i in range(1, len(pink_noise), 2):
+    for i in range(1,len(pink_noise),2):
         y_data.append(pink_noise[i])
-    for i in range(0, len(pink_noise), 2):
+    for i in range(0,len(pink_noise),2):
         x_data.append(pink_noise[i])
     x_data = ratio_0_to_100(x_data)
     y_data = ratio_0_to_100(y_data)
 
     return x_data, y_data
 
-
 def trigonometry(Orientation, TD):
     y_diff = np.sin(Orientation) * TD
     x_diff = np.cos(Orientation) * TD
     return x_diff, y_diff
-
 
 def pink_noise_travel_distance_and_orientation(N):
     TD = cn.powerlaw_psd_gaussian(1, N)
@@ -136,17 +105,15 @@ def pink_noise_travel_distance_and_orientation(N):
 
     return x_data, y_data
 
-
 def pink_noise_and_derivative(N):
-    pink_signal = cn.powerlaw_psd_gaussian(1, N)
-    derivative = lib.derivative(pink_signal, 1)
+    pink_signal = cn.powerlaw_psd_gaussian(1,N)
+    derivative = lib.derivative(pink_signal,1)
     derivative = list(derivative)
     derivative.append(derivative[-1])
     x_data = ratio_0_to_100(pink_signal)
     y_data = ratio_0_to_100(derivative)
 
     return x_data, y_data
-
 
 def lorenz(xyz, *, s=10, r=28, b=2.667):
     """
@@ -163,12 +130,11 @@ def lorenz(xyz, *, s=10, r=28, b=2.667):
        Values of the Lorenz attractor's partial derivatives at *xyz*.
     """
     x, y, z = xyz
-    x_dot = s * (y - x)
-    y_dot = r * x - y - x * z
-    z_dot = x * y - b * z
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
 
     return np.array([x_dot, y_dot, z_dot])
-
 
 def lorenz_x_data_y_data(dt, N):
     xyzs = np.empty((N + 1, 3))
@@ -181,14 +147,12 @@ def lorenz_x_data_y_data(dt, N):
 
     return x_data, y_data
 
-
 def aizawa(state, a=0.95, b=0.7, c=0.6, d=3.5, e=0.25, f=0.1):
     x, y, z = state
     dx = (z - b) * x - d * y
     dy = d * x + (z - b) * y
     dz = c + a * z - (z ** 3) / 3 - (x ** 2 + y ** 2) * (1 + e * z) + f * z * x ** 3
     return np.array([dx, dy, dz])
-
 
 def aizawa_x_data_y_data(dt, N):
     xyzs = np.empty((N + 1, 3))
@@ -203,150 +167,57 @@ def aizawa_x_data_y_data(dt, N):
 
     return x_data, y_data
 
-
 def erase_attractor_values(attractor, desired_number):
     attractor = list(attractor)
-    cut_number = int(len(attractor) / desired_number)
+    cut_number = int(len(attractor)/desired_number)
     new_attractor = attractor[::cut_number]
 
     return new_attractor
 
-
 def pink_noise_x_and_y(N):
-    """ This function creates 2 different and seperate pink noise signals
-    I suggest this approach but with a correction from the grip code"""
-    x_data = pink_noise_signal_creation_using_cn(N)
-    y_data = pink_noise_signal_creation_using_cn(N)
+    """ This function creates 2 different and seperate pink noise signals"""
+    x_data = cn.powerlaw_psd_gaussian(1, N)
+    y_data = cn.powerlaw_psd_gaussian(1, N)
     x_data = ratio_0_to_100(x_data)
     y_data = ratio_0_to_100(y_data)
 
     return x_data, y_data
 
-def pink_noise_signal_creation_using_cn(N):
-    pink = False
-    iterations = 0
-    while pink == False:
-
-        pink_noise = cn.powerlaw_psd_gaussian(1, N)
-
-
-        slope, positive_freqs_log, positive_magnitude_log, intercept, name, r, p, positive_freqs, positive_magnitude = quality_assessment_of_temporal_structure_FFT_method(pink_noise, 'pink_noise_z')
-
-        if round(slope, 2) == -0.5 and (p < 0.05) and (r <= -0.7):
-            #
-            # Figure of Frequincies vs Magnitude
-            # plt.figure(figsize=(10,6))
-            # plt.plot(positive_freqs, positive_magnitude)
-            # plt.title(f'{name}\nFFT of Sine Wave')
-            # plt.xlabel('Frequency (Hz)')
-            # plt.ylabel('Magnitude')
-            # plt.grid()
-            # plt.show()
-
-            # plt.figure(figsize=(10, 6))
-            # plt.scatter(positive_freqs_log, positive_magnitude_log, label='Log-Log Data', color='blue')
-            # plt.plot(positive_freqs_log, slope * positive_freqs_log + intercept,
-            #          label=f'Fit: \nSlope = {slope:.2f}\nr = {r}\np = {p}', color='red')
-            # plt.title(f'{name}\nLog-Log Plot of FFT (Frequency vs Magnitude)')
-            # plt.xlabel('Log(Frequency) (Hz)')
-            # plt.ylabel('Log(Magnitude)')
-            # plt.legend()
-            # plt.grid()
-            # plt.show()
-            pink = True
-        else:
-            print('Not valid pink noise signal')
-            iterations +=1
-            print(iterations)
-
-    return pink_noise
-
-
-def quality_assessment_of_temporal_structure_FFT_method(signal, name):
-    # Apply FFT
-    fft_output = np.fft.fft(signal)  # FFT of the signal
-    fft_magnitude = np.abs(fft_output)  # Magnitude of the FFT
-
-    # Calculate frequency bins
-    frequencies = np.fft.fftfreq(len(signal), d=1/0.01)  # Frequency bins
-
-    # Keep only the positive frequencies
-    positive_freqs = frequencies[1:len(frequencies) // 2]  # Skip the zero frequency
-    positive_magnitude = fft_magnitude[1:len(frequencies) // 2]  # Skip the zero frequency
-
-    #  Figure of Frequincies vs Magnitude
-    # plt.figure(figsize=(10,6))
-    # plt.plot(positive_freqs, positive_magnitude)
-    # plt.title(f'{name}\nFFT of Sine Wave')
-    # plt.xlabel('Frequency (Hz)')
-    # plt.ylabel('Magnitude')
-    # plt.grid()
-    # plt.show()
-
-    positive_freqs_log = np.log10(positive_freqs[positive_freqs > 0])
-    positive_magnitude_log = np.log10(positive_magnitude[positive_freqs > 0])
-
-    r, p = pearsonr(positive_freqs_log, positive_magnitude_log)
-
-    # Perform linear regression (best fit) to assess the slope
-    slope, intercept, r_value, p_value, std_err = stats.linregress(positive_freqs_log, positive_magnitude_log)
-    print(f'r_value = {r_value}')
-    print(f'p_value = {p_value}')
-
-    # Plot the log-log results
-    # plt.figure(figsize=(10,6))
-    # plt.scatter(positive_freqs_log, positive_magnitude_log, label='Log-Log Data', color='blue')
-    # plt.plot(positive_freqs_log, slope * positive_freqs_log + intercept, label=f'Fit: \nSlope = {slope:.2f}\nr = {r}\np = {p}', color='red')
-    # plt.title(f'{name}\nLog-Log Plot of FFT (Frequency vs Magnitude)')
-    # plt.xlabel('Log(Frequency) (Hz)')
-    # plt.ylabel('Log(Magnitude)')
-    # plt.legend()
-    # plt.grid()
-    # plt.show()
-
-    return slope, positive_freqs_log, positive_magnitude_log, intercept, name, r, p, positive_freqs, positive_magnitude
-
-
 def convert_force_values(data_point):
     """
     This function takes data points of Kinvent force and returns force in kg
     """
-    data_point = data_point / 10
+    data_point = data_point/10
     return data_point
-
 
 def convert_quaternion(quaternion):
     """
     This function takes data points of Kinvent IMU quaternions and returns quaternion from -1 to 1
     """
-    quaternion = (quaternion - 32768) / 16384
+    quaternion = (quaternion - 32768)/16384
     return quaternion
-
 
 def convert_acceleration(acc):
     """
     This function takes data points of Kinvent force and returns force in m/(s^2)
     """
-    acc = (acc - 32768) * (16 / 32768)
+    acc = (acc - 32768) * (16/32768)
     acc = acc * g
     return acc
-
 
 def convert_ang_vel(vel):
     """
     This function takes data points of Kinvent IMU angular velocity and returns angular velocity in the range of +-2000°/s
     """
-    vel = (vel - 32768) * (2000.0 / 32768)
+    vel = (vel - 32768) * (2000.0/32768)
     return vel
-
 
 def convert_magnetic_field_density(mag):
     """
     This function takes data points of Kinvent IMU angular velocity and returns angular velocity in the range of +-2000°/s
     """
-    mag = (mag - 32768) * (2500.0 / 32768)
+    mag = (mag - 32768) * (2500.0/32768)
     return mag
-
 
 def q_to_ypr(q):
     """ Import q as a list of quaternion"""
@@ -358,8 +229,7 @@ def q_to_ypr(q):
         roll = math.degrees(roll)
         pitch = math.degrees(pitch)
         return [yaw, pitch, roll]
-
-
+    
 def convert_KIVNENT_IMU_to_readable_file(df):
     columns_IMU = ['Time', 'Q_0', 'Q_1', 'Q_2', 'Q_3', 'Acc_X', 'Acc_Y', 'Acc_Z', 'Ang_Vel_X', 'Ang_Vel_Y', 'Ang_Vel_Z',
                    'Mag_X', 'Mag_Y', 'Mag_Z']
@@ -401,63 +271,19 @@ def convert_KIVNENT_IMU_to_readable_file(df):
     df['Roll'] = roll
 
     return df
-
-
+    
 def values_during_game(df):
     """
     This function takes the whole data frame and returns only the rows where the target_pos_y are not None.
-    This way we only take the rows in which we have targets
-    """
+    This way we only take the rows in which we have targets"""
+    df = converting_str_into_float(df, 'target_pos_y')
 
-    filtered_data = df[df['target_pos_y'] != ' None']
-    target_pos_x = filtered_data['target_pos_x'].to_numpy()
-    target_pos_y = filtered_data['target_pos_y'].to_numpy()
-    player_pos_x = filtered_data['player_pos_x'].to_numpy()
-    player_pos_y = filtered_data['player_pos_y'].to_numpy()
+    filtered_data = df[df['target_pos_y'] != None]
+    print('Hello')
+    filtered_data = converting_str_into_float(filtered_data, 'target_pos_y')
 
-    target_pos_x = converting_str_into_float(target_pos_x)
-    target_pos_y = converting_str_into_float(target_pos_y)
-    player_pos_x = converting_str_into_float(player_pos_x)
-    player_pos_y = converting_str_into_float(player_pos_y)
+    return filtered_data
 
-    return target_pos_x, target_pos_y, player_pos_x, player_pos_y
-
-
-def converting_str_into_float(time_series):
-    time_series = time_series.astype(float)
-    return time_series
-
-
-def return_the_values_before_target_change(target_pos_x, target_pos_y, player_pos_x, player_pos_y):
-    indexes_before_change = find_the_last_moment_before_target_change_position(target_pos_x)
-    target_pos_x = target_pos_x[indexes_before_change]
-    target_pos_y = target_pos_y[indexes_before_change]
-    player_pos_x = player_pos_x[indexes_before_change]
-    player_pos_y = player_pos_y[indexes_before_change]
-    print(len(target_pos_x))
-
-    return target_pos_x, target_pos_y, player_pos_x, player_pos_y
-
-
-def find_the_last_moment_before_target_change_position(target_pos_x):
-    indexes_before_change = []
-    for i in range(len(target_pos_x) - 1):
-        if target_pos_x[i] != target_pos_x[i + 1]:
-            indexes_before_change.append(i)
-    indexes_before_change.append(len(target_pos_x) - 1)
-
-    return indexes_before_change
-
-
-def spatial_error_calculation(target_pos_x, target_pos_y, player_pos_x, player_pos_y):
-    spatial_error = np.sqrt((player_pos_x - target_pos_x) ** 2 + (player_pos_y - target_pos_y) ** 2)
-
-    return spatial_error
-
-
-def create_excel_file(x_data, y_data, directory, name):
-    dist = {'X coordinates': x_data,
-            'Y coordinates': y_data}
-
-    excel = pd.DataFrame(dist)
-    excel.to_excel(fr'{directory}\{name}.xlsx')
+def converting_str_into_float(df, column_name):
+    df["FloatColumn"] = pd.to_numeric(df[column_name], errors="coerce")  # Invalid values become NaN
+    return df
